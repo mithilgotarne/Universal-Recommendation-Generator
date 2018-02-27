9fcbc6a27d977a8caed83dc3a6222b2228e83c65
mithil <mithilgotarne@gmail.com>
2018-02-28 00:02:36 +0530

    some changes

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -23,10 +23,11 @@ export class ProductComponent implements OnInit {
           continue;
         } else if (this.product._source.hasOwnProperty(key)) {
           const element = this.product._source[key];
-          if (['Product Dimensions0', 'Product Dimensions1', 'Product Dimensions2'].includes(key)) {
+          if (['Product Dimensions0', 'Product Dimensions1', 'Product Dimensions2',
+          'Package Dimensions0', 'Package Dimensions1', 'Package Dimensions2' ].includes(key)) {
             pd +=  element + ' x ';
-            if ('Product Dimensions2' === key) {
-              this.content += pd.slice(0, pd.length - 2) + 'cm';
+            if (['Product Dimensions2', 'Package Dimensions2'].includes(key)) {
+              this.content += pd.slice(0, pd.length - 2) + 'cm <br>';
             }
             continue;
           }
