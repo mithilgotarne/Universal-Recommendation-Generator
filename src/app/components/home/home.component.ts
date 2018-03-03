40a2b0518668fd48ba3272ddc98efc1b45c2d552
mithil <mithilgotarne@gmail.com>
2018-03-03 11:55:28 +0530

    animations added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('indexAnim', [
+      transition('* => *', [
+        query('mat-card', style({ opacity: 0, transform: 'translateX(-40px)' })),
+
+        query('mat-card', stagger('500ms', [
+          animate('800ms 1.2s ease-out', style({ opacity: 1, transform: 'translateX(0)' })),
+        ])),
+
+        query('mat-card', [
+          animate(1000, style('*'))
+        ])
+
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
   selectedIndex: any;
